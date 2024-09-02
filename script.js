443d8d1074316cfcda8ddf39899d5a4296c9d9ce
vineandrade <vineandrade18@gmail.com>
2024-09-02 15:11:54 -0300

    Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function adicionarNumero(numero) {
+    operacaoAtual += numero;
+    atualizarTela (operacaoAtual)
+}
+function adicionarOperacao(operacao) {
+    if (operacaoAtual !==''){
+        operador = operacao;
+        operacaoAtual += operacao;
+        atualizarTela(operacaoAtual);
+    }
+}
+function calcular() {
+    try {
+        resultado = eval(operacaoAtual);
+        atualizarTela(resultado);
+        operacaoAtual = resultado.toString();
+    } catch (e){
+        atualizarTela('erro');
+        operacaoAtual='';
+    }
+}
+function limpar() {
+    operacaoAtual='';
+    operador='';
+    resultado=0;
+    atualizarTela(0);
+}
+function atualizarTela(valor) {
+    document.getElementById('resultado').innerHTML=valor;
+}
